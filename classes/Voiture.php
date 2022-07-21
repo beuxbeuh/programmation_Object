9ceb34327649b49bf2017c65c2720e334188d0a6
Akame <Akame@localhost>
2022-07-21 11:08:06 +0200

    ajout d'une fonction dans la classe voiture

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -1,8 +1,14 @@
-// les fichiers de classes commencent par comnvention avec une majuscule
 <?php
+// les fichiers de classes commencent par comnvention avec une majuscule
+
 
 class Voiture {
 	public $marque;
 	public $couleur;
-}
+	public string $bande;
+	public int $nbrRous = 4;
 
+	public function demarage() {
+	    echo "La voiture démarre lentenement";
+	}
+}
--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
 
 require_once './classes/Voiture.php';
+require ('function/affichage.php');
 
+$voiture1 = new Voiture();
+$voiture1->couleur = "rouge";
+$voiture1->marque = "BMW";
+
+var_dump($voiture1);
+
+$voiture2 = new Voiture();
+$voiture2->couleur = "bleue";
+$voiture2->marque = "Peugeot";
+$voiture2->couleur = "gris";
+$voiture2->bande = "avant vers arriere au milieu";
+$voiture2->demarage();
+dump($voiture2);
