1252e23ae68e3fc560b24f42ef3b7d8cf73f9195
Akame <Akame@localhost>
2022-07-21 12:10:32 +0200

    ajout des fonctions construct et destruct

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -1,14 +1,49 @@
 <?php
-// les fichiers de classes commencent par comnvention avec une majuscule
+// les fichiers de classes commencent par convention avec une majuscule
 
 
 class Voiture {
-	public $marque;
-	public $couleur;
-	public string $bande;
+	private $marque;
+	private $couleur;
 	public int $nbrRous = 4;
 
-	public function demarage() {
-	    echo "La voiture démarre lentenement";
+	public function __construct(string $couleur, string $marque)
+	{
+	    $this->couleur = $couleur;
+	    $this->marque = $marque;
+	}
+
+	public function get_mark(): string
+	{
+	     return $this->marque; // Méthod pour obtenir la marque de la voiture
+	}
+
+	public function set_mark(string $marque): void
+	{
+	     $this->marque = $marque; // Méthod pour definir la marque de la voiture
+	}
+
+	public function get_color(): string
+	{
+	    return $this->couleur;
+	}
+
+	public function set_color(string $couleur): void
+	{
+	    $this->couleur = $couleur;
+	}
+
+	public function demarage(): void
+	{
+	    echo "La voiture démarre lentement";
+	}
+
+	public function affichageCouleur():string
+	{
+	    return '<p>La voiture est de couleur ' . $this->get_color() . '</p>';
+	}
+	public function __destruct()
+	{
+	    
 	}
 }
--- index.php
+++ index.php
@@ -3,16 +3,30 @@
 require_once './classes/Voiture.php';
 require ('function/affichage.php');
 
-$voiture1 = new Voiture();
-$voiture1->couleur = "rouge";
-$voiture1->marque = "BMW";
+/* $voiture1 = new Voiture();
+/* $voiture1->couleur = "rouge";
+  $voiture1->marque = "BMW";
+$voiture1->set_mark("BMW");
+echo $voiture1->get_mark();
+echo '<br>';
+$voiture1->set_color("rouge");
+echo $voiture1->affichageCouleur();
+echo '<br>';
 
-var_dump($voiture1);
+// var_dump($voiture1);
 
 $voiture2 = new Voiture();
+$voiture2->set_color("gris");
+
+echo $voiture2->affichageCouleur(); */
+/*
 $voiture2->couleur = "bleue";
 $voiture2->marque = "Peugeot";
 $voiture2->couleur = "gris";
-$voiture2->bande = "avant vers arriere au milieu";
-$voiture2->demarage();
-dump($voiture2);
+$voiture2->demarage(); */
+// dump($voiture2);
+
+$voiture3 = new Voiture("bleue ciel", "Dacia");
+dump($voiture3);
+
+
